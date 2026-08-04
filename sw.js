f422d969e6ceb2347e114da3238078e637cd0c92
officialwinmyat-WMA <officialwinmyat@gmail.com>
2026-08-05 00:29:23 +0630

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker Installing...');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
